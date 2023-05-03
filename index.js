a167c4f90b4735ab8c48adba87494dd66c6e4389
Zachary Mowatt <zamowatt@gmail.com>
2023-05-03 13:13:44 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply
+const num1 = 2
+const num2 = 31
+multiply = num1*num2
 
-// Write your code here
+
+let random
+random = Math.ceil(Math.random()+1);
+
+
+let mod 
+const num3 = 604;
+const num4 = 10;
+mod = num3 % num4
+
+let max
+max = Math.max(2, 4, 20, 16, 3, 17, 12)
